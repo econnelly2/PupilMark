281c56f1c1b6e25d5bf194dd916399bc0a59ea2e
Elliott Connelly <econnelly@stkentigernsacademy.org.uk>
2015-11-24 10:29:33 +0000

    sprint1.1

--- PUPIL.java
+++ PUPIL.java
@@ -1,5 +1,10 @@
 public class PUPIL 
 {
+    //list of properties 
+    private String fname;
+    private String sname;
+    private float mark;
+    
    public PUPIL()
    {
        //constructor
