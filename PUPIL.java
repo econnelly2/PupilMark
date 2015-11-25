bcbcd052c77ac668ac0f024f774bbf01ce1e3213
Elliott Connelly <econnelly@stkentigernsacademy.org.uk>
2015-11-25 12:02:36 +0000

    sprint 1.5

--- PUPIL.java
+++ PUPIL.java
@@ -5,16 +5,37 @@ public class PUPIL
     private String sname;
     private float mark;
     
-   public PUPIL()
-   {
-       //constructor
+     public PUPIL()
+        {
+           //constructor
        //initialise instance vars
        fname = "";
        sname = "";
        mark = 0.0f;
+     }
+       public void readPupilsMark(String dataItems)
+       {
+        //unoaxk string of row data into fields 
+        String[] rowItems = dataItems.split(",");
+        
+        // store each data item as instance property 
+        fname = rowItems [0];
+        sname = rowItems [1];
+        mark = Float.parseFloat(rowItems[2]);
+        
+        // produce name & mark of highest mark; 
     }
-    
-}
-    
-    
+    public String writeDetails ()
+    {
+        //join up data into a string to output as a row 
+        // use "," to separate csv columns 
+        String memberData = "";
+        memberData = memberData.concat(fname);
+        memberData = memberData.concat(",");
+        memberData = memberData.concat(sname);
+        memberData = memberData.concat(",");
+        memberData = memberData.concat(Float.toString(mark));
+        return memberData;
+    }   
+ }  
     
